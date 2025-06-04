9340d2668910b460c527b7a4ba6fe89452945323
Denis Ok <denis@getdefacto.com>
2025-06-04 11:13:48 +0800

    Fix GH page deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // To make it work when deployed to GitHub Pages
+  base: "./",
   root: "./src",
   build: {
     outDir: "../dist",
